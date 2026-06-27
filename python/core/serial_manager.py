5ae5e6b4672098e92905cb3443b34e2ab5f2ede5
Alex Sun <alexsun0725@gmail.com>
2026-06-27 03:18:34 -0500

    feat: improved structure and created core classes

---
+++ python/core/serial_manager.py
@@ -0,0 +1,18 @@
+# reads serial, handles TX/RX to BCD
+
+import serial
+
+class SerialManager:
+    def __init__(self, port_name):
+        # Connect to serial
+        self._ser = serial.Serial(port_name, 115200, timeout=1)
+        print(f"Connected to {port_name}\n")
+        self._ser.write("RST\n".encode("utf-8")) # restart BCD for cleaner time
+
+    def read_line(self):
+        # RX from serial
+        return self._ser.readline()
+
+    def write_command(self, command_string):
+        # TX over serial
+        self._ser.write(command_string.encode("utf-8"))
