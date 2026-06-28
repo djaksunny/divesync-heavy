8006635eb432a41c116338dc33b0a6ac0e9d44d1
Alex Sun <alexsun0725@gmail.com>
2026-06-27 21:53:54 -0500

    feat: finalized basic structure, manual control only

--- python/main.py
+++ python/main.py
@@ -1,4 +1,68 @@
 # Main orchestrator file
 
-def main():
-    pass
+import time
+
+# Core packages
+from core.experiment import Experiment
+from core.serial_manager import SerialManager
+from core.logger import Logger
+from core.telemetry import Telemetry
+
+# Controller packages
+from controllers.manual import ManualController
+# from controllers.manual import PIDController
+# from controllers.manual import RLController
+
+# Visualizer packages
+# from visualization.analysis import Analysis
+# from visualization.live_plotter import LivePlotter
+
+# Create experiment instance
+exp = Experiment()
+exp.setup_experiment()
+
+# Ensure not empty
+if exp.com_port is None:
+    exit()
+
+# Create serial manager instance
+ser = SerialManager(exp.com_port)
+
+# Create logger instance
+log = Logger(exp._folder_path)
+
+# Create telemetry instance
+tel = Telemetry()
+
+# Create controllers
+match exp.mode:
+    case "manual":
+        con = ManualController()
+    # case "pid":
+    #     con = PIDController()
+    # case "rl":
+    #     con = RLController()
+
+# Boot/handshake protocol
+while not(exp.is_ready()):
+    exp.handshake_protocol(ser.read_line())
+
+# Begin experiment
+exp.start()
+
+# Timing setup
+loop_hz = 20
+dt = 1/ loop_hz
+last = time.time()
+
+while exp.is_running():
+    if time.time() - last >= dt:
+        last = time.time()
+        try:
+            line = ser.read_line()
+            if exp.is_valid_csv(line):
+                tel.update(line)
+                log.write_raw(line)
+                ser.write_command(con.get_command())
+        except KeyboardInterrupt:
+            exp.abort()
--- python/run_experiment.py
+++
@@ -1,6 +0,0 @@
-# Experiment launcher
-
-from main import main
-
-if __name__ == "__main__":
-    main()
