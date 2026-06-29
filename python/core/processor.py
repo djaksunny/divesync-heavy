0dc5695894d01af047980cbee30d0432721e8db2
Alex Sun <alexsun0725@gmail.com>
2026-06-29 15:15:53 -0500

    feat: added processor and updated logger accordingly

---
+++ python/core/processor.py
@@ -0,0 +1,29 @@
+# Processes telemetry object to contain processed depth (median filtering) and actuator displacement (scaled to mm)
+
+class Processor:
+    def __init__(self):
+        self._window = []
+        self._LIMIT = 15
+
+        self.depth_filtered_m = None
+        self.actuator_mm = None
+        self.processed_csv = None
+
+    def process(self, tel):
+        # Update window
+        self._window.append(tel.depth_m)
+
+        if len(self._window) > self._LIMIT:
+            self._window.pop(0)
+
+        # Median filter
+        sorted_window = sorted(self._window)
+
+        mid = len(sorted_window) // 2
+        self.depth_filtered_m = sorted_window[mid]
+
+        # Actuator units conversion
+        self.actuator_mm = tel.actuator_raw * 50 / 4095
+
+        # Saves CSV for logger
+        self.processed_csv = f"{self.depth_filtered_m},{self.actuator_mm}"
