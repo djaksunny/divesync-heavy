7ee05a45194674aa4db40a28356b6b07ff1ff558
Alex Sun <alexsun0725@gmail.com>
2026-06-30 17:13:35 -0500

    feat: added waveform and polished files

--- python/core/processor.py
+++ python/core/processor.py
@@ -1,10 +1,11 @@
-# Processes telemetry object to contain processed depth (median filtering) and actuator displacement (scaled to mm)
+# Processes telemetry object to contain processed depth (median filtering) and actuator displacement (scaled to mm), along with setpoints
 
 class Processor:
     def __init__(self):
         self._window = []
         self._LIMIT = 15
 
+        self.time_s = None
         self.depth_filtered_m = None
         self.depth_setpoint_m = None
         self.actuator_mm = None
@@ -31,5 +32,8 @@ class Processor:
         self.depth_setpoint_m = depth_setpoint_m
         self.actuator_setpoint_mm = actuator_setpoint_mm  
 
+        # Update time
+        self.time_s = tel.time_ms / 1000
+
         # Saves CSV for logger
-        self.processed_csv = f"{self.depth_filtered_m},{self.depth_setpoint_m},{self.actuator_mm},{self.actuator_setpoint_mm}"
+        self.processed_csv = f"{self.time_s},{self.depth_filtered_m},{self.depth_setpoint_m},{self.actuator_mm},{self.actuator_setpoint_mm},{tel.motor_cmd}"
