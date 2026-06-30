c9d8d041806462b2daf1ba28d6cc3532f11fd9f1
Alex Sun <alexsun0725@gmail.com>
2026-06-30 15:42:40 -0500

    fix: updated classes to accommodate future additions (outer PID)

--- python/controllers/manual.py
+++ python/controllers/manual.py
@@ -8,7 +8,7 @@ class ManualController:
         self._joystick = pygame.joystick.Joystick(0)
         self._joystick.init()
 
-    def get_command(self, setpoint=None, actuator_mm=None):
+    def get_command(self, actuator_mm=None, actuator_setpoint_mm=None, depth_m=None, depth_setpoint_m=None):
         pygame.event.pump()
         pwm = round(255 * self._joystick.get_axis(1))
         return f"U:{pwm}\n"
--- python/controllers/pid.py
+++ python/controllers/pid.py
@@ -1,31 +1,25 @@
 from simple_pid import PID
-import time
 
 class PIDController:
-    def __init__(self, Kp, Ki, Kd, low_mm, high_mm, period_s):
-        self._pid = PID(Kp, Ki, Kd)
-        self._pid.output_limits = (-255, 255)
-        self._pid.sample_time = 0.05
+    def __init__(self, inner_gains, outer_gains=None):
+        Kp, Ki, Kd = inner_gains
+        self._inner = PID(Kp, Ki, Kd)
+        self._inner.output_limits = (-255, 255)
+        self._inner.sample_time = 0.05
 
-        self._low_mm = low_mm
-        self._high_mm = high_mm
-        self._period_s = period_s
-        self._last_switch_time = time.time()
-        self._is_high = False
+        self._outer = None
+        if outer_gains:
+            Kp_o, Ki_o, Kd_o = outer_gains
+            self._outer = PID(Kp_o, Ki_o, Kd_o)
+            self._outer.sample_time = 0.05
 
-        self.current_setpoint = low_mm  # exposed for logging
+    def get_command(self, actuator_mm=None, actuator_setpoint_mm=None, depth_m=None, depth_setpoint_m=None):
+        if self._outer:
+            self._outer.setpoint = depth_setpoint_m
+            actuator_setpoint_mm = self._outer(depth_m)
+        else:
+            actuator_setpoint_mm = actuator_setpoint_mm  # passed directly in inner-only mode
 
-    def _get_setpoint(self):
-        if time.time() - self._last_switch_time >= self._period_s:
-            self._is_high = not self._is_high
-            self._last_switch_time = time.time()
-        return self._high_mm if self._is_high else self._low_mm
-
-    def set_state(self, actuator_mm):
-        self._actuator_mm = actuator_mm
-
-    def get_command(self):
-        self.current_setpoint = self._get_setpoint()
-        self._pid.setpoint = self.current_setpoint
-        pwm = round(self._pid(self._actuator_mm))
+        self._inner.setpoint = actuator_setpoint_mm
+        pwm = round(self._inner(actuator_mm))
         return f"U:{pwm}\n"
