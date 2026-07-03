005d876511e9feb3c43426b044688008688cbfdd
Alex Sun <alexsun0725@gmail.com>
2026-07-03 17:05:51 -0500

    fix: updated gains and added feedforward for actuator deadband

--- python/controllers/inner.py
+++ python/controllers/inner.py
@@ -1,10 +1,14 @@
 from simple_pid import PID
 
 class InnerPIDController:
-    def __init__(self, gains):
+    def __init__(self, gains, deadband=None):
         Kp, Ki, Kd = gains
+        if deadband:
+            self._deadband = deadband
+        else:
+            self._deadband = 0
         self._pid = PID(Kp, Ki, Kd)
-        self._pid.output_limits = (-255, 255)
+        self._pid.output_limits = (-255+self._deadband, 255-self._deadband)
         self._pid.sample_time = 0.05
 
     def get_command(self, actuator_mm, actuator_setpoint_mm):
@@ -15,4 +19,10 @@ class InnerPIDController:
 
         self._pid.setpoint = actuator_setpoint_mm
         pwm = round(self._pid(actuator_mm))
+        if pwm >= -2 and pwm <= 2:
+            pwm = 0
+        elif pwm > 2:
+            pwm += self._deadband
+        else:
+            pwm -= self._deadband
         return f"U:{pwm}\n"
